e0ab4853aa0da956b9a15354329140419b4ca7d2
Igor Afonso <igor.henrique.afonso99@gmail.com>
2025-09-04 22:29:47 -0300

    feat: creating base controller for map all requests

---
+++ ProjectManagementAPI/ProjectManagementAPI/Config/ServiceCollection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace ProjectManagementAPI.Config
+{
+    public static class ServiceCollection
+    {
+        public static void ApplicationServices(this IServiceCollection builder)
+        {
+            
+        }
+    }
+}
