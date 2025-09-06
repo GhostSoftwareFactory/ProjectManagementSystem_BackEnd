ace1932a72ebeb43ee3628d585a3e466bfaee00f
Igor Afonso <igor.henrique.afonso99@gmail.com>
2025-09-05 22:09:53 -0300

    feat: creting new DTOs for new entities

---
+++ ProjectManagementAPI/ProjectManagementAPI/Models/DTO/CardDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectManagementAPI.Models.DTO
+{
+    public class CardDTO
+    {
+        public Guid? CardID { get; set; }
+        public Guid? ColumnID { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string? CardTitle { get; set; }
+        public string? CardContent { get; set; }
+    }
+}
